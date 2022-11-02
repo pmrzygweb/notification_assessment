74f6dc284edea8fb97751e5e1a8be00e1dd3b8a8
Phillip Mrzyglocki <pmrzyg.upwork@gmail.com>
2022-11-02 10:30:59 -0400

    initial codebase

---
+++ backend/database/migrations/2022_11_02_135425_create_logs_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLogsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('logs', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained();
+            $table->string('message_type');
+            $table->text('message_content');
+            $table->string('notification_type');
+            $table->string('status');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('logs');
+    }
+}
